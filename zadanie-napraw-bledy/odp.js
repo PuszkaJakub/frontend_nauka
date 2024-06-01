48bb10c0a9d3cca7eb69e2b40628d02ac90eb81b
Jakub Puszka <126493564+PuszkaJakub@users.noreply.github.com>
2024-06-01 17:14:01 +0200

    "Zadanie – Napraw błędy"
    Moi drodzy,
    
    Niżej znajdziecie kod JS, który musicie naprawić. 🙂
    
    Wykorzystajcie do tego wiedzę zdobytą w tym dziale, a w kolejnym odcinku pokażę Wam, jak należało poprawnie wykonać całe zdanie!
    
    Ilość błędów do naprawy: 6
    
    
    
    Kod:
    const firstName = 'Tomek'
    let age = 12
    age = 18
     
    var favColor = 'niebieski'
    const fav-meal = 'schabowy'
     
    let current.Car
    current.Car = 'Audi'
     
    let firstName = 'Ania'
    let age2 = 24
    let ulubionyKolor = 'czerwony'

---
+++ zadanie-napraw-bledy/odp.js
@@ -0,0 +1,13 @@
+let firstName = 'Tomek'
+let age = 12
+age = 18
+ 
+const favColor = 'niebieski'
+const favMeal = 'schabowy'
+ 
+let currentCar
+currentCar = 'Audi'
+ 
+firstName = 'Ania'
+let age2 = 24
+let ulubionyKolor = 'czerwony'
